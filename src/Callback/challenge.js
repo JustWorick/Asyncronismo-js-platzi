791b113986442fa2973f9c2f7ce7913a53b4383c
Abraham Fortunatte <a.fortunatte@gmail.com>
2023-03-22 05:58:22 -0300

    challenge, fetch data class(error)

--- src/Callback/challenge.js
+++ src/Callback/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 // const XMLHttppRequest = require('xmlhttprequest').XMLHttpRequest; (por si sale error, este codigo puede ser el bueno)
 const API = 'https:api.escuelajs.co/api/v1';
 
@@ -10,14 +10,27 @@ function fetchData(urlApi, callback) {
         if (xhttp.readystate === 4) {
             if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
+            }else {
+                const error = new Error('Error' + urlApi);
+                return callback(error, null);
             }
-        } else {
-            const error = new Error('Error' + urlApi);
-            return callback(error, null);
-        }
+        } 
     }
     xhttp.send();
 }
+
+fetchData(`${API}/products`, function (error1, data1) {
+    if (error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
+        if (error2) return console.error(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
+            if (error3) return console.error(error3);
+            console.log(data1[0]);
+            console.log(data2.title);
+            console.log(data3.name);
+        });
+    });
+});
 
 // 𝗖𝗹𝗮𝘀𝗲 #𝟳: 𝗫𝗠𝗟𝗛𝗧𝗧𝗣𝗥𝗲𝗾𝘂𝗲𝘀𝘁𝟳/𝟮𝟭 📤
 // .
