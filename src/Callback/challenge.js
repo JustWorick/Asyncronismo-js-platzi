d38c002cb539b78b96a4aa6aa406052b244c302b
Abraham Fortunatte <a.fortunatte@gmail.com>
2023-03-24 05:41:47 -0300

    se arreglo el problema de el callback y se agrego el index de promise

--- src/Callback/challenge.js
+++ src/Callback/challenge.js
@@ -1,20 +1,19 @@
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-// const XMLHttppRequest = require('xmlhttprequest').XMLHttpRequest; (por si sale error, este codigo puede ser el bueno)
-const API = 'https:api.escuelajs.co/api/v1';
+const API = 'https://api.escuelajs.co/api/v1';
 
 function fetchData(urlApi, callback) {
     let xhttp = new XMLHttpRequest();
 
-    xhttp.open('Get', urlApi, true);
+    xhttp.open('GET', urlApi, true);
     xhttp.onreadystatechange = function (event) {
-        if (xhttp.readystate === 4) {
+        if (xhttp.readyState === 4) {
             if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
-            }else {
+            } else {
                 const error = new Error('Error' + urlApi);
                 return callback(error, null);
             }
-        } 
+        }
     }
     xhttp.send();
 }
@@ -23,7 +22,7 @@ fetchData(`${API}/products`, function (error1, data1) {
     if (error1) return console.error(error1);
     fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
         if (error2) return console.error(error2);
-        fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
+        fetchData(`${API}/categories/${data2.category.id}`, function (error3, data3) {
             if (error3) return console.error(error3);
             console.log(data1[0]);
             console.log(data2.title);
---
+++ src/promise/index.js
@@ -0,0 +1,24 @@
+//
+const promise = new Promise(function (resolve, reject) {
+    resolve('hey!')
+})
+
+const cows = 54;
+
+const countcows = new Promise(function (resolve, reject) {
+    if (cows > 10) {
+        resolve(`We have ${cows} cows on the farm`);
+    } else {
+        reject('There is no cows on the farm')
+    }
+});
+
+countcows.then((result) => {
+    console.log(result);
+})
+//la promesa por si sola entregara solo 1 resultado, para que tome el segundo es necerario agregar el .catch
+.catch((b) => {
+    console.log(b);
+})
+//cuando la promesa termine, ya sea, resuelta o rechazada se puede encadenar un accion con .finally
+.finally(() => console.log('Finally'));
